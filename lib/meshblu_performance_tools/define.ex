5b2a6c06ef5d269e9da99602decb824f0e70d775
enixdark <cqshinn92@gmail.com>
2017-06-06 22:11:59 +0700

    delete redunt files that after refactoring

--- lib/meshblu_performance_tools/define.ex
+++
@@ -1,5 +0,0 @@
-# {:ok, p} = MeshbluPerformanceTools.Register.start_link []
-# MeshbluPerformanceTools.Register.register(p)
-
-# {:ok, pid} = HTTPSender.receive_request
-# HTTPSender.send_request("", pid)
--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule MeshbluPerformanceTools.Mixfile do
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
     [
-      extra_applications: [:logger,:httpotion, :poison, :ibrowse, :poolboy],
+      extra_applications: [:logger,:httpotion, :poison, :ibrowse, :poolboy ,:gen_mqtt, :gen_fsm],
       mod: {MeshbluPerformanceTools, []}
     ]
   end
@@ -40,7 +40,13 @@ defmodule MeshbluPerformanceTools.Mixfile do
       {:poolboy, "~> 1.5.1"},
       {:ibrowse, "~> 4.4.0"},
       {:hackney, "~> 1.8"},
-      {:logger_file_backend, "~> 0.0.9"}
+      {:logger_file_backend, "~> 0.0.9"},
+      {:gen_mqtt, "~> 0.3.1"},
+      {:cowboy, "~> 1.1.2"},
+      {:plug, "~> 1.3.5"},
+      # {:gen_stage, "~> 0.12.0"},
+      {:gen_fsm, "~> 0.1.0"},
+      # {:gen_state_machine, "~> 2.0"}
     ]
   end
 end
