4362ee1c51dd94d8f8805bb61285318cffca5119
enixdark <cqshinn92@gmail.com>
2017-06-12 10:30:14 +0700

    update docker-compose and README to run tool

--- README.md
+++ README.md
@@ -4,22 +4,22 @@
 
 ## Setup
 
-###Requirement
+### Requirement
 
 - Elixir v1.4.x
 - Nodejs 
 - Python 2.7.x
-- Mongodb
-- Redis-server
 
 ### How to Install app
 
 install all package for elixir app
 
 `mix deps.get`
+
 compile to generate tasks 
 
 `mix compile`
+
 generate binary file through escript
 
 `mix escript.build`
@@ -31,11 +31,34 @@ use command to test
 `meshblu_performance_tools mqtt`
 
 check help through command
+
 `meshblu_performance_tools -h`
 
 if you want to test in with mix you can use command:
 
 `mix cli httt` or `mix http`
 
-`mix cli mqtt` or `mix mqtt` 
+`mix cli mqtt` or `mix mqtt`
 
+
+For http, currently, it's only support subscriber with 2 mode:
+
+- unique: use a uuid and token of a device that have registered
+- file: load a uuid and token of a device or devices that u define in file, for sample format file, please check it in samples/data
+
+Example:
+
+to create a request http to meshblu streaming server with 1 concurency request (-c options) / 1 total request (-n options ) and delay every concurency request in 2s (-d options)
+for -f options, it'll force a devices or a collections devices load repeat many times if if total uuid/token of devices < totol connection
+
+`mix http -m unique b37b29d6-2f62-48c8-84a5-7ea5477235db:259f6a54f27477ffc63342d2f1614f788bd9724a -c 1 -f -n 1 -d 2000`
+
+or 
+
+`meshblu_performance_tools http -m unique b37b29d6-2f62-48c8-84a5-7ea5477235db:259f6a54f27477ffc63342d2f1614f788bd9724a -c 1 -f -n 1 -d 2000`
+
+or 
+
+`
+mix http -m file /home/cqshinn/GIT/meshblu_project/meshblu_performance_tools/samples/files/devices.yaml -c 10
+`
--- lib/mix/tasks/mqtt.ex
+++ lib/mix/tasks/mqtt.ex
@@ -11,22 +11,35 @@ defmodule Mix.Tasks.Mqtt do
     "mqtt"
   end
 
-  defp process(opts) do
+  # defp process(opts) do
+  #   case opts do
+  #     [concurrency: concurrency, delay: delay, max_connection: max_connection, uri: uri] -> 
+  #       Logger.info "Start services..................."
+  #       turn = max_connection / concurrency - 1
+  #       for _ <- 0..round(turn) do
+  #         for _ <- 0..round(concurrency) do 
+  #           {:ok, pid} =  MeshbluPerformanceTools.MQTT.Client.start_link
+  #           MeshbluPerformanceTools.MQTT.Client.subscriber(pid)
+  #         end
+  #         :timer.sleep(delay)
+  #       end
+  #      _ ->
+  #        :ok
+  #   end
+  #   loop()
+  # end
+
+  defp process(opts, [head|data], count, persistent_auth) do
     case opts do
-      [concurrency: concurrency, delay: delay, max_connection: max_connection, uri: uri] -> 
-        Logger.info "Start services..................."
-        turn = max_connection / concurrency - 1
-        for _ <- 0..round(turn) do
-          for _ <- 0..round(concurrency) do 
-            {:ok, pid} =  MeshbluPerformanceTools.MQTT.Client.start_link
-            MeshbluPerformanceTools.MQTT.Client.subscriber(pid)
-          end
-          :timer.sleep(delay)
-        end
+      [concurrency: concurrency, delay: delay, level: _, max_connection: max_connection, mode: :unique, uri: uri] ->        
+        :ok
+      [concurrency: concurrency, delay: delay, level: :once, max_connection: _, mode: _, uri: uri] ->        
+        :ok
+      [concurrency: concurrency, delay: delay, force: _, level: :multi, max_connection: max_connection, mode: _, uri: uri] -> 
+        :ok
        _ ->
          :ok
     end
-    loop()
   end
 
 end
